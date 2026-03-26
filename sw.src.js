6de562ddbab4d3958775c4cdc26b8bdb2e52a150
hef <hef@pbrfrat.com>
2026-03-25 23:37:28 -0500

    feat(p2p): enabling p2p features of libp2p

--- sw.src.js
+++ sw.src.js
@@ -1,41 +1,60 @@
-import { bootstrap } from '@libp2p/bootstrap';
+import { bitswap, trustlessGateway } from '@helia/block-brokers';
+import { httpGatewayRouting, libp2pRouting } from '@helia/routers';
+import { createHelia, libp2pDefaults } from 'helia';
+import { createLibp2p } from 'libp2p';
 import { createVerifiedFetch } from '@helia/verified-fetch';
+import { IDBBlockstore } from 'blockstore-idb';
+import { IDBDatastore } from 'datastore-idb';
+
+const GATEWAYS = [
+    'https://trustless-gateway.link',
+    'https://dweb.link',
+    'https://w3s.link',
+    'https://nftstorage.link',
+    'https://4everland.io',
+];
+
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (event) => event.waitUntil(clients.claim()));
 self.addEventListener('fetch', (() => {
-    let vFetch;
+    let vFetchPromise;
+    function getVFetch() {
+        if (!vFetchPromise) {
+            vFetchPromise = (async () => {
+                const blockstore = new IDBBlockstore('helia/blocks');
+                const datastore = new IDBDatastore('helia/data');
+                await blockstore.open();
+                await datastore.open();
+
+                const libp2p = await createLibp2p(libp2pDefaults());
+
+                const helia = await createHelia({
+                    blockstore,
+                    datastore,
+                    libp2p,
+                    blockBrokers: [
+                        bitswap(),
+                        trustlessGateway(),
+                    ],
+                    routers: [
+                        libp2pRouting(libp2p),
+                        httpGatewayRouting({ gateways: GATEWAYS }),
+                    ],
+                });
+
+                return createVerifiedFetch(helia);
+            })();
+        }
+        return vFetchPromise;
+    }
     return (event) => {
         const url = new URL(event.request.url);
         const isSameOrigin = url.origin === self.location.origin;
         if (isSameOrigin) {
             event.respondWith((async () => {
-                if (!vFetch) vFetch = await createVerifiedFetch({
-                    gateways: [
-                        'https://trustless-gateway.link',
-                        'https://dweb.link',
-                        'https://w3s.link',
-                        'https://nftstorage.link',
-                        'https://4everland.io',
-                    ],
-                    routers: [
-                        'https://delegated-ipfs.dev',
-                        'https://cid.contact',
-                    ],
-                    libp2pConfig: {
-                        peerDiscovery: [
-                            bootstrap({
-                                list: [
-                                    '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
-                                    '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
-                                    '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt',
-                                    '/dnsaddr/va1.bootstrap.libp2p.io/p2p/12D3KooWKnDdG3iXw9eTFijk3EWSunZcFi54Zka4wmtqtt6rPxc8',
-                                ],
-                            }),
-                        ],
-                    },
-                });
+                const vFetch = await getVFetch();
                 const p2pUrl = `ipns://${self.location.hostname}${url.pathname}${url.search}${url.hash}`;
-                return await vFetch(p2pUrl);
+                return vFetch(p2pUrl);
             })());
         }
     };
